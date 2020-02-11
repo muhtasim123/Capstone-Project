e49605401a967338ed431cf72102fac58d638115
Mohammad Shahriyar <100587434@oncampus.local>
2020-02-11 13:30:40 -0500

    test

--- caregiverlogin.php
+++ caregiverlogin.php
@@ -1,3 +1,8 @@
+<?php
+	session_start();
+	require_once('dbconfig/config.php');
+	//phpinfo();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +16,42 @@
 				<img src="logo100.png" alt="Avatar" class="avatar">
 			</div>
 		<form action="index.php" method="post">
+<?php
+			if(isset($_POST['login']))
+			{
+				@$username=$_POST['username'];
+				@$password=$_POST['password'];
+				$query = "select * from caregiver where name='$username' and password='$password' ";
+				//echo $query;
+				$query_run = mysqli_query($con,$query);
+				//echo mysql_num_rows($query_run);
+				if($query_run)
+				{
+					if(mysqli_num_rows($query_run)>0)
+					{
+					$row = mysqli_fetch_array($query_run,MYSQLI_ASSOC);
 
+					$_SESSION['name'] = $username;
+					$_SESSION['password'] = $password;
+
+					//header( "Location: homepage.php");
+					echo '<script type="text/javascript">alert("Database Worked")</script>';
+
+					}
+					else
+					{
+						echo '<script type="text/javascript">alert("No such User exists. Invalid Credentials")</script>';
+					}
+				}
+				else
+				{
+					echo '<script type="text/javascript">alert("Database Error")</script>';
+				}
+			}
+			else
+			{
+			}
+		?>
 			<div class="inner_container">
 				<label><b>Username</b></label>
 				<input type="text" placeholder="Enter Username" name="username" required>
