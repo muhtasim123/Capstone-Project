2404d011d87aca3693eb10312fa4031d733fc641
Ubuntu <ubuntu@ip-172-31-94-120.ec2.internal>
2020-02-11 17:39:09 +0000

    test

--- caregiverlogin.php
+++ caregiverlogin.php
@@ -1,9 +1,3 @@
-<?php
-	session_start();
-	require_once('dbconfig/config.php');
-	//phpinfo();
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,42 +21,6 @@
 				<a href="register.php"><button type="button" class="register_btn">Register</button></a>
 			</div>
 		</form>
-
-		<?php
-			if(isset($_POST['login']))
-			{
-				@$username=$_POST['username'];
-				@$password=$_POST['password'];
-				$query = "select * from userinfotbl where username='$username' and password='$password' ";
-				//echo $query;
-				$query_run = mysqli_query($con,$query);
-				//echo mysql_num_rows($query_run);
-				if($query_run)
-				{
-					if(mysqli_num_rows($query_run)>0)
-					{
-					$row = mysqli_fetch_array($query_run,MYSQLI_ASSOC);
-
-					$_SESSION['username'] = $username;
-					$_SESSION['password'] = $password;
-
-					header( "Location: homepage.php");
-					}
-					else
-					{
-						echo '<script type="text/javascript">alert("No such User exists. Invalid Credentials")</script>';
-					}
-				}
-				else
-				{
-					echo '<script type="text/javascript">alert("Database Error")</script>';
-				}
-			}
-			else
-			{
-			}
-		?>
-
 	</div>
 </body>
 </html>
