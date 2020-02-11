288e8f4d76a9af71c6c5fcae50f7a4288ecc4312
Mohammad Shahriyar <100587434@oncampus.local>
2020-02-11 12:14:39 -0500

    adding front page

---
+++ frontpage.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="frontpage.css">
+<body>
+<a href="<?php echo "header.php"; ?>"
+<div class="split left">
+  <div class="parent">
+    <div>
+      <img src="logo100.png" class="image1">
+    </div>
+  </div>
+  <div class="centered">
+    <h1>STAFF</h1>
+  </div>
+</div>
+</a>
+<a href="<?php echo "careGiverLogin.php" ?>"
+<div class="split right">
+  <div class="parent">
+    <div>
+      <img src="logo100.png" class="image2">
+    </div>
+  </div>
+  <div class="centered">
+    <h1>CARE GIVER</h1>
+  </div>
+</div>
+</a>
+</div>
+</body>
+</html>
