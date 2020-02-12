40cb7229e97053daf9b3ad920d985d2ba8d2f54a
Mohammad Shahriyar <100587434@oncampus.local>
2020-02-12 18:48:47 -0500

    testing things

--- admin/update-profile.php
+++ admin/update-profile.php
@@ -14,7 +14,7 @@ if(isset($_POST['Submit']))
 	$question3=$_POST['question3'];
 	
   $uid=intval($_GET['uid']);
-$query=mysqli_query($con,"update patient set fname='$fname' ,lname='$lname', question1=$question1, question2=$question2, question3=$question3 where id='$uid';");
+$query=mysqli_query($con,"update patient set fname='$fname' ,lname='$lname', question1=$question1, question2=$question2, question3=$question3 where id='$uid'");
 
 $_SESSION['msg']="Profile Updated successfully";
 }
