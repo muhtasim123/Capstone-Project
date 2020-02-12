26256b9d037e3967fd42ac6d63ff9cc5efb47bd9
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-02-12 18:27:39 -0500

    Update update-profile.php

--- admin/update-profile.php
+++ admin/update-profile.php
@@ -15,7 +15,10 @@ if(isset($_POST['Submit']))
 	
   $uid=intval($_GET['uid']);
 $query=mysqli_query($con,"update patient set fname='$fname' ,lname='$lname', question1=$question1, question2=$question2, question3=$question3 where id='$uid';");
-$_SESSION['msg']="Profile Updated successfully";
+
+	if ($conn->query($sql) === TRUE) {
+		$_SESSION['msg']="Profile Updated successfully";
+	}
 }
 ?>
 
