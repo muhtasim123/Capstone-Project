9efbf6c26401b13256ee3c621b8657b5b6ddccb2
Mohammad Shahriyar <100587434@oncampus.local>
2020-02-12 17:34:04 -0500

    test

--- admin/stafflogin.php
+++ admin/stafflogin.php
@@ -1,28 +1,7 @@
 <?php
 session_start();
 include("dbconnection.php");
-if(isset($_POST['login']))
-{
-  $adminusername=$_POST['username'];
-  $pass=$_POST['password'];
-$ret=mysqli_query($con,"SELECT * FROM staff WHERE name='$adminusername' and password='$pass'");
-$num=mysqli_fetch_array($ret);
-if($num>0)
-{
-$extra="manage-users.php";
-$_SESSION['login']=$_POST['username'];
-$_SESSION['id']=$num['id'];
-echo "<script>window.location.href='".$extra."'</script>";
-exit();
-}
-else
-{
-$_SESSION['action1']="*Invalid username or password";
-$extra="stafflogin.php";
-echo "<script>window.location.href='".$extra."'</script>";
-exit();
-}
-}
+
 
 ?>
 
@@ -39,15 +18,50 @@ exit();
 			<div class="imgcontainer">
 				<img src="logo100.png" alt="Avatar" class="avatar">
 			</div>
-		<form action="manage-users.php" method="post">
+		<form action="" method="post">
+<?php
+if(isset($_POST['login']))
+			{
+				@$username=$_POST['username'];
+				@$password=$_POST['password'];
+				$query = "select * from staff where name='$username' and password='$password' ";
+				//echo $query;
+				$query_run = mysqli_query($con,$query);
+				//echo mysql_num_rows($query_run);
+				if($query_run)
+				{
+					if(mysqli_num_rows($query_run)>0)
+					{
+					$row = mysqli_fetch_array($query_run,MYSQLI_ASSOC);
 
+					$_SESSION['name'] = $username;
+					$_SESSION['password'] = $password;
+
+					header( "Location: manage-users.php");
+					echo '<script type="text/javascript">alert("Database Worked")</script>';
+
+					}
+					else
+					{
+						echo '<script type="text/javascript">alert("No such staff exists. Invalid Credentials")</script>';
+					}
+				}
+				else
+				{
+					echo '<script type="text/javascript">alert("Database Error")</script>';
+				}
+			}
+			else
+			{
+			}
+			?>
 			<div class="inner_container">
 				<label><b>Username</b></label>
 				<input type="text" placeholder="Enter Username" name="username" required>
 				<label><b>Password</b></label>
 				<input type="password" placeholder="Enter Password" name="password" required>
 				<button class="login_button" name="login" type="submit">Login</button>
-				<a href="register.php"><button type="button" class="register_btn">Register</button></a>
+				
 				<a href="frontpage.php"><button type="button" class="back_btn">Back</button></a>
 			</div>
 		</form>
