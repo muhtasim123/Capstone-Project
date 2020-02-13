e91125d01ef9818a6f204eae5ea47fe7bfcbc0c3
Mohammad Shahriyar <100587434@oncampus.local>
2020-02-13 13:29:56 -0500

    testing

--- admin/manage-patients.php
+++ admin/manage-patients.php
@@ -85,9 +85,9 @@ include'dbconnection.php';
                   <div class="col-md-12">
                       <div class="content-panel">
                           <table class="table table-striped table-advance table-hover">
-	                  	  	  <h4><i class="fa fa-angle-right"></i> All User Details </h4>
+	                  	  	  <h4><i class="fa fa-angle-right"></i> All Patient Details </h4>
 							  <ul class="nav pull-right top-menu">
-                    <li><a class="logout" href="new-patient.php" style="padding-top:40px";>Add Patients</a></li>
+                    <li><a class="logout" href="new-patient.php" style="margin-top:-35px";>Add Patient</a></li>
             	</ul>
 							  
 	                  	  	  <hr>
