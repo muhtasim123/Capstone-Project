2f910d228eb02df406f7200590e9a4a13825f7e1
Mohammad Shahriyar <100587434@oncampus.local>
2020-02-12 19:23:25 -0500

    testing

--- homepage.php
+++ homepage.php
@@ -37,11 +37,8 @@
 		<center><h2>Home Page</h2></center>
 		<center><h3>Welcome <?php echo $_SESSION['name']; ?></h3></center>
 <?php
-		
-		$firstname=$_SESSION['name'];?>
-		<p>Name: <?phpecho $firstname;?></p>
-		<?php
-		$ret=mysqli_query($con,"select * from patient where fname=$firstname");
+		$firstname=$_SESSION['name'];
+		$ret=mysqli_query($con,"select * from patient where fname=$_SESSION['firstname']");
 	  while($row=mysqli_fetch_array($ret))
 	  
 	  {?>
