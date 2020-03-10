f4df1188ee1a3ae30a7fd260b8c0219cd4ae551c
Mohammad Shahriyar <100587434@oncampus.local>
2020-03-10 12:57:12 -0400

    testing

--- frontpage.php
+++ frontpage.php
@@ -60,10 +60,9 @@ session_start();
 
                 </div>
 			
-				<a href="<?php echo "caregiverlogin.php"; ?>">
+				
                 <div class="col-lg-4 col-md-6">
-			
-                                 
+                                 <a  style="text-decoration:none" href="<?php echo "caregiverlogin.php"; ?>">
                     <div class="single_service">
                          <div class="service_thumb service_icon_bg_1 d-flex align-items-center justify-content-center">
                              <div class="service_icon">
