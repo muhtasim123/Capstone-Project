311865c4a96842b5fb3e0138dd9f650d5b982e9e
Nafis Mobassher <100587562@oncampus.local>
2020-03-15 19:35:03 -0400

    nafis

--- homepage.php
+++ homepage.php
@@ -98,24 +98,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
 <?php } } ?>
       <center><h2>Upload a file</h2></center>
 
-			<?php
-if(isset($_POST['submit'])){
-
- // Count total files
- $countfiles = count($_FILES['file']['name']);
-
- // Looping all files
- for($i=0;$i<$countfiles;$i++){
-  $filename = $_FILES['file']['name'][$i];
-
-  // Upload file
-  move_uploaded_file($_FILES['file']['tmp_name'][$i],'upload/'.$filename);
-
- }
-}
-?>
         <center><form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST"></center><br><br>
-            <center><input name="file[]" type="file"><input type="submit" value="Upload" name="submit"></center>
+            <center><input name="userfile" type="file"><input type="submit" value="Upload"></center>
         </form>
 
 				<?php
