baa0be8b2dc9ff746a3b31a3522f3200ab63e352
Mohammad Shahriyar <100587434@oncampus.local>
2020-03-16 22:20:49 -0400

    testing

--- admin/new-staff.php
+++ admin/new-staff.php
@@ -5,31 +5,137 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Sign Up Page</title>
-<link rel="stylesheet" href="css/style1.css">
-</head>
-<body style="background-color:#bdc3c7">
-	<div id="main-wrapper">
-	<center><h2>Staff Sign Up Form</h2></center>
-		<form action="new-staff.php" method="post">
-			<div class="imgcontainer">
-				<img src="logo100.png" alt="Avatar" class="avatar">
-			</div>
-			<div class="inner_container">
-				<label><b>Username</b></label>
-				<input type="text" placeholder="Enter Username" name="username" required>
-				<label><b>Email</b></label>
-				<input type="text" placeholder="Enter Username" name="username" required>
-				<label><b>Password</b></label>
-				<input type="password" placeholder="Enter Password" name="password" required>
-				<label><b>Confirm Password</b></label>
-				<input type="password" placeholder="Enter Password" name="cpassword" required>
-				<button name="register" class="sign_up_btn" type="submit">Sign Up</button>
 
-				<a href="manage-patients.php"><button type="button" class="back_btn">Back to Dashboard</button></a>
-			</div>
-		</form>
+		
+<head>
+<html lang="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Admin | Create Profile</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  <section id="container" >
+      <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <a href="#" class="logo"><b>Staff Dashboard</b></a>
+            <div class="nav notify-row" id="top_menu">
+
+
+
+                </ul>
+            </div>
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <ul class="sidebar-menu" id="nav-accordion">
+
+              	  <p class="centered"><a href="#"><img src="assets/img/logo100.png" class="img-circle" width="100"></a></p>
+              	  <h5 class="centered"><?php echo $_SESSION['login'];?></h5>
+
+                  <li class="mt">
+                      <a href="change-password.php">
+                          <i class="fa fa-file"></i>
+                          <span>Change Password</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="manage-patients.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Manage Patients</span>
+                      </a>
+
+                  </li>
+				  
+				   <li class="sub-menu">
+                      <a href="new-staff.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Add Staff</span>
+                      </a>
+
+                  </li>
+				  
+				  <li class="sub-menu">
+                      <a href="new-caregiver.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Add Caregiver</span>
+                      </a>
+
+                  </li>
+
+
+              </ul>
+          </div>
+      </aside>
+
+      <section id="main-content">
+          <section class="wrapper">
+          	<h3><i class="fa fa-angle-right"></i> Staff Sign Up Form</h3>
+
+				<div class="row">
+
+
+
+                  <div class="col-md-12">
+                      <div class="content-panel">
+                      <p align="center" style="color:#F00;"><?php echo $_SESSION['msg'];?><?php echo $_SESSION['msg']=""; ?></p>
+                           <form class="form-horizontal style-form" name="form1" method="post" action="" onSubmit="return valid();">
+                           <p style="color:#F00"><?php echo $_SESSION['msg'];?><?php echo $_SESSION['msg']="";?></p>
+                          <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Username</label>
+                              <div class="col-sm-10">
+                                  <input type="text" placeholder="Enter Username" class="form-control" name="username" required>
+                              </div>
+                          </div>
+
+                              <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Email</label>
+                              <div class="col-sm-10">
+                                  <input type="text" placeholder="Enter Email" class="form-control" name="email" required>
+                              </div>
+                          </div>
+
+                               <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Password</label>
+                              <div class="col-sm-10">
+                                  <input type="text" placeholder="Enter Password" class="form-control" name="password" required>
+                              </div>
+                          </div>
+						  
+						  <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Confirm Password</label>
+                              <div class="col-sm-10">
+                                  <input type="text" placeholder="Enter Password" class="form-control" name="cpassword" required >
+                              </div>
+                          </div>
+						  
+                          <div style="margin-left:100px;">
+						  <button name="register" type="submit" class="btn btn-theme">Sign Up</button>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+		</section>
+      </section></section>
+		
+	
 
 		<?php
 			if(isset($_POST['register']))
