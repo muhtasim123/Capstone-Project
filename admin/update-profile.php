a7db358d4edd6f7953d5f788f606159bacdf2ac2
Mohammad Shahriyar <100587434@oncampus.local>
2020-03-16 22:35:19 -0400

    testing

--- admin/update-profile.php
+++ admin/update-profile.php
@@ -137,8 +137,8 @@ if(isset($_POST['submit']))
                      
                          <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Question 1</label>
                               <div class="col-sm-10">
-                                  <input type="text" class="form-control" name="question1" value="question1" >
-								  <input type="text" class="form-control" name="answer" value="answer1" >
+                                  <input type="text" class="form-control" name="question1" value="<?php echo $row['question1'];?>">
+								  <input type="text" class="form-control" name="answer" value="<?php echo $row['answer1'];?>">
                               </div>	  
                 </div>
                             <div class="form-group">
