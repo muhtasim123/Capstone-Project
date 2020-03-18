9782e64d8e22a68f38a0ca4fb4aa95c6a80e95c2
Mohammad Shahriyar <100587434@oncampus.local>
2020-03-17 20:48:35 -0400

    testing

--- admin/new-patient.php
+++ admin/new-patient.php
@@ -13,12 +13,7 @@ if(isset($_POST['submit']))
 	$question3=$_POST['question3'];
 	$question4=$_POST['question4'];
 	$question5=$_POST['question5'];
-	$answer1=$_POST['answer1'];
-	$answer2=$_POST['answer2'];
-	$answer3=$_POST['answer3'];
-	$answer4=$_POST['answer4'];
-	$answer5=$_POST['answer5'];
-	$query=mysqli_query($con,"INSERT INTO patient (fname, lname, question1, question2, question3, question4, question5, answer1, answer2, answer3, answer4, answer5, datejoined) VALUES ($fname, $lname, $question1, $question2, $question3, $question4, $answer1, $answer2, $answer3, $answer4, $answer5, CURRENT_TIMESTAMP)") ;
+	$query=mysqli_query($con,"INSERT patient set fname='$fname' ,lname='$lname', question1='$question1', question2='$question2', question3='$question3', question4='$question4', question5='$question5', datejoined=CURRENT_TIMESTAMP");
 
 	if($query)
 		{
@@ -145,35 +140,30 @@ if(isset($_POST['submit']))
 			<div class="form-group" id="q1Type">
 			<div class="col-sm-10">
 				<input id="question1" type="text" class="form-control" name="question1">
-				<input id="answer1" type="text" class="form-control" name="answer1">
 				</div>
 			</div>
 
 			<div class="form-group" id="q2Type" style="display:none;">
 			<div class="col-sm-10">
 				<input id="question2" type="text" class="form-control" name="question2">
-				<input id="answer2" type="text" class="form-control" name="answer2">
 			</div>
 			</div>
 
 			<div class="form-group" id="q3Type" style="display:none;">
 			<div class="col-sm-10">
 				<input id="question3" type="text" class="form-control" name="question3">
-				<input id="answer3" type="text" class="form-control" name="answer3">
 			</div>
 			</div>
 			
 			<div class="form-group" id="q4Type" style="display:none;">
 			<div class="col-sm-10">
 				<input id="question4" type="text" class="form-control" name="question4">
-				<input id="answer4" type="text" class="form-control" name="answer4">
 			</div>
 			</div>
 			
 			<div class="form-group" id="q5Type" style="display:none;">
 			<div class="col-sm-10">
 				<input id="question5" type="text" class="form-control" name="question5">
-				<input id="answer5" type="text" class="form-control" name="answer5">
 			</div>
 			</div>
 
