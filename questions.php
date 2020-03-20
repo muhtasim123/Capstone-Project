f74b52ffad369f4f751122a6cbda76554878bffe
Mohammad Shahriyar <100587434@oncampus.local>
2020-03-20 11:20:08 -0400

    testing

--- questions.php
+++ questions.php
@@ -114,12 +114,12 @@ if(isset($_POST['update']))
               </ul>
           </div>
       </aside>
-      <?php 
-    	$pid=$_SESSION['pid'];
-	$query="select * from patient where id='$pid'";
-	$query_run = mysqli_query($con,$query);	
-	  if($query_run)
+  
+		  <?php $ret=mysqli_query($con,"select * from patient where id='$pid'");
+	  while($row=mysqli_fetch_array($ret))
+
 	  {?>
+	  
       <section id="main-content">
           <section class="wrapper">
 		  <?php $row=mysqli_fetch_array($query_run); ?>
