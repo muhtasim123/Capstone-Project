c0f0e62484949e161b760af7cb205db23ce500bf
Nafis Mobassher <100587562@oncampus.local>
2020-03-19 23:51:19 -0400

    nafis

--- admin/new-caregiver.php
+++ admin/new-caregiver.php
@@ -169,12 +169,13 @@
 								<?php $ret=mysqli_query($con,"select * from patient");
 								while($row=mysqli_fetch_array($ret))
 								{
-									$drop_down = $row['fname']['lname'];
+									$drop_down = $row['fname'];
 									echo "<option value='$drop_down'>$drop_down</option>";
-								}?>
-							</div>
-								</div>
+								}
+								?>
 							</select>
+						</div>
+							</div>
 
 									<div class="form-group">
 										<div class="col-sm-10">
