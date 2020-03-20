57c58b0ab43c3b162851267cb29315e24fcdffba
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-03-19 22:17:32 -0400

    Update questions.php

--- questions.php
+++ questions.php
@@ -112,12 +112,13 @@ if(isset($_POST['submit']))
       </aside>
       <?php 
     	$pid=$_SESSION['pid'];
-	$ret=mysqli_query($con,"select * from patient where id='$pid'");	
-	$row=mysqli_fetch_array($ret);
-	  if ($ret)
+	$query="select * from patient where id='$pid'";
+	$query_run = mysqli_query($con,$query);	
+	  if ($query_run)
 	  {?>
       <section id="main-content">
           <section class="wrapper">
+		  <?php $row=mysqli_fetch_array($query_run); ?>
           	<h3><i class="fa fa-angle-right"></i> <?php echo $row['fname'];?> <?php echo $row['lname'];?>'s Information</h3>
 
 				<div class="row">
