e5480f5174623ce86cf0a0f5ee65fd4dd350bd05
Mohammed Hameeduddin <45569565+100587893@users.noreply.github.com>
2020-03-20 13:56:34 -0400

    Update albumadmin.php

--- admin/albumadmin.php
+++ admin/albumadmin.php
@@ -21,7 +21,7 @@
   <!-- Navbar -->
   <div class="w3-top" >
     <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-        <a href="#home" class="w3-bar-item w3-button"><b>Capstone Project</b></a>
+        <a href="https://ontario-shores.herokuapp.com/admin/manage-patients.php" class="w3-bar-item w3-button"><b>Manage Patients</b></a>
         <!--Float to the right, hide in small screen -->
         <div class="w3-right w3-hide-small">
           <a href="#projects" class="w3-bar-item w3-button">Projects</a>
