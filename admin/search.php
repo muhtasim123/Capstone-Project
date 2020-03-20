82c1d26c3a6383248519d85fb802e63c2c564545
Mohammed Hameeduddin <45569565+100587893@users.noreply.github.com>
2020-03-20 13:15:35 -0400

    Add files via upload

---
+++ admin/search.php
@@ -0,0 +1,269 @@
+<?php
+
+$connect = mysqli_connect("us-cdbr-iron-east-04.cleardb.net", "bc9da719e482f3", "deea7ef6", "heroku_dbefbfd5b04ac35");
+
+function picture_query($connect)
+{
+ $profile = $_GET['profileid'];
+ $query = "SELECT * FROM profile_data WHERE type='picture' AND tag LIKE '%{$profile}%' ORDER BY id ASC"; 
+ $result = mysqli_query($connect, $query);
+ return $result;
+}
+
+function video_query($connect)
+{
+ $profile = $_GET['profileid'];
+ $query = "SELECT * FROM profile_data WHERE type='video' AND tag LIKE '%{$profile}%' ORDER BY id ASC"; 
+ $result = mysqli_query($connect, $query);
+ return $result;
+}
+
+function audio_query($connect)
+{
+ $profile = $_GET['profileid'];
+ $query = "SELECT * FROM profile_data WHERE type='music' AND tag LIKE '%{$profile}%' ORDER BY id ASC"; 
+ $result = mysqli_query($connect, $query);
+ return $result;
+}
+
+function make_slide_indicators($connect)
+{
+ $output = '';
+ $count = 0;
+ $result = picture_query($connect);
+ while($row = mysqli_fetch_array($result))
+ {
+  if($count == 0)
+  {
+   $output .= '
+   <li data-target="#dynamic_slide_show" data-slide-to="'.$count.'" class="active"></li>
+   ';
+  }
+  else
+  {
+   $output .= '
+   <li data-target="#dynamic_slide_show" data-slide-to="'.$count.'"></li>
+   ';
+  }
+  $count = $count + 1;
+ }
+ return $output;
+}
+
+function make_slides($connect)
+{
+ $output = '';
+ $count = 0;
+ $result = picture_query($connect);
+ while($row = mysqli_fetch_array($result))
+ {
+  if($count == 0)
+  {
+   $output .= '<div class="item active">';
+  }
+  else
+  {
+   $output .= '<div class="item">';
+  }
+  $output .= '
+   <img src="data/'.$row["url"].'" alt="'.$row["id"].'" style="margin:auto;width:800px;height:700px;"/>
+   <div class="carousel-caption">
+    <h3>'.$row["url"].'</h3>
+   </div>
+  </div>
+  ';
+  $count = $count + 1;
+ }
+ return $output;
+}
+
+function make_video($connect)
+{
+ $output = '';
+ $count = 0;
+ $result = video_query($connect);
+ while($row = mysqli_fetch_array($result))
+ {
+  if($count == 0)
+  {
+   $output .= '<div class="item active">';
+  }
+  else
+  {
+   $output .= '<div class="item">';
+  }
+  $output .= '
+   <video src="data/'.$row["url"].'" alt="'.$row["id"].'" class="vid" controls/>
+   <div class="carousel-caption">
+    <h3>'.$row["url"].'</h3>
+   </div>
+  </div>
+  ';
+  $count = $count + 1;
+ }
+ return $output;
+}
+
+function make_audio($connect)
+{
+ $output = '';
+ $count = 0;
+ $result = audio_query($connect);
+ while($row = mysqli_fetch_array($result))
+ {
+  if($count == 0)
+  {
+   $output .= '<div class="item active">';
+  }
+  else
+  {
+   $output .= '<div class="item">';
+  }
+  $output .= '
+   <audio controls src="data/'.$row["url"].'" alt="'.$row["id"].'" class="audio"/>
+   <div class="carousel-caption">
+    <h3>'.$row["url"].'</h3>
+   </div>
+  </div>
+  ';
+  $count = $count + 1;
+ }
+ return $output;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Slideshow</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script>
+  $(document).ready(function() {
+	$(".begin").click(function(){
+		$("#dynamic_slide_show").carousel('cycle');
+	});
+	$(".pause").click(function() {
+		$("#dynamic_slide_show").carousel('pause');
+	});
+  });
+  </script>
+  <style>
+	.box{
+		width: 70%;
+		height: 200px;
+		margin:auto;}
+	.begin{
+		height: 50px;
+		width: 50px;}
+	.pause{
+		height: 50px;
+		width: 50px;}
+	.control{
+		width: 10%;
+		margin:auto;}
+	.vid{
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%; }
+	.audio{
+		display: block;
+		margin:auto;
+		width: 30%;}
+  </style>
+ </head>
+ <body  style="background-color:#919191">
+  <br />
+  <?php $profile = $_GET['profileid']; ?>
+ <div class="box">
+     <!--<div class="container">-->
+       <h2 align="center">Pictures in Profile: <?php echo ucfirst($profile); ?></h2>
+       <br />
+       <div id="dynamic_slide_show" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+        <?php echo make_slide_indicators($connect); ?>
+        </ol>
+    
+        <div class="carousel-inner">
+         <?php echo make_slides($connect); ?>
+        </div>
+        <a class="left carousel-control" href="#dynamic_slide_show" data-slide="prev">
+         <span class="glyphicon glyphicon-chevron-left"></span>
+         <span class="sr-only">Previous</span>
+        </a>
+    
+        <a class="right carousel-control" href="#dynamic_slide_show" data-slide="next">
+         <span class="glyphicon glyphicon-chevron-right"></span>
+         <span class="sr-only">Next</span>
+        </a>
+       </div>
+       
+       <div class="control"><br>
+
+        <button type="button" class="begin" >
+        <span class="glyphicon glyphicon-play" ></span></button>
+        <button type="button" class="pause">
+        <span class="glyphicon glyphicon-pause" ></span></button>
+       </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+   <h2 align="center">Videos in Profile: <?php echo ucfirst($profile); ?></h2>
+<br>
+<div id="dynamic_slide_show2" class="carousel slide">
+	<div class="carousel-inner">
+    	<?php echo make_video($connect); ?>
+	</div>
+    
+  		<a class="left carousel-control" href="#dynamic_slide_show2" data-slide="prev">
+         <span class="glyphicon glyphicon-chevron-left"></span>
+         <span class="sr-only">Previous</span>
+        </a>
+    
+        <a class="right carousel-control" href="#dynamic_slide_show2" data-slide="next">
+         <span class="glyphicon glyphicon-chevron-right"></span>
+         <span class="sr-only">Next</span>
+        </a>
+	</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+   <h2 align="center">Music in Profile: <?php echo ucfirst($profile); ?></h2>
+<br>
+<div id="dynamic_slide_show3" class="carousel slide">
+	<div class="carousel-inner">
+    	<?php echo make_audio($connect); ?>
+	</div>
+    
+  		<a class="left carousel-control" href="#dynamic_slide_show3" data-slide="prev">
+         <span class="glyphicon glyphicon-chevron-left"></span>
+         <span class="sr-only">Previous</span>
+        </a>
+    
+        <a class="right carousel-control" href="#dynamic_slide_show3" data-slide="next">
+         <span class="glyphicon glyphicon-chevron-right"></span>
+         <span class="sr-only">Next</span>
+        </a>
+    </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</body>
+</html>
---
+++ admin/secondpage.php
@@ -0,0 +1,195 @@
+<?php 
+session_start();
+//creating a connection
+$mysqli = new mysqli("us-cdbr-iron-east-04.cleardb.net", "bc9da719e482f3", "deea7ef6", "heroku_dbefbfd5b04ac35");
+
+//checking connection
+if($mysqli->connect_error){
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+$currAlbum = $_REQUEST['currentAlbum'];
+$currProfile = $_REQUEST['currentProfileID'];
+
+
+$redirectURL = "/";
+$redirectTime = 3;
+
+function checkAlbumSelected($currprof, $curralbm, &$redirurl, &$redirtime) {
+    if(!isset($_REQUEST['selectedAlbum'])) {
+        $redirurl = "albumgallery.php?profileid={$currprof}&albumname={$curralbm}";
+        $redirtime = 5;
+        
+        echo "<p>No target album was selected. Going back.</p>";
+        echo "<br/>";
+        echo "<a href='albumgallery.php?profileid={$currProfile}&albumname={$curralbm}'>Click here if the browser is not automatically redirecting in {$redirectTime} seconds.</a>";
+    }
+}
+
+function transformGalleryID($gallerySelected) {
+    return array_map(function($galleryid) {
+        return $_SESSION['galleryDataID'][$galleryid];
+    }, $gallerySelected);
+    
+}
+
+
+function transformGalleryURL($gallerySelected) {
+    return array_map(function($galleryid) {
+        return $_SESSION['galleryDataURL'][$galleryid];
+    }, $gallerySelected);
+    
+}
+
+
+
+if(!isset($_REQUEST['gallery'])) {
+    $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
+    $redirectTime = 5;
+    echo "<p>No image was selected. Going back.</p>";
+    echo "<br/>";
+    echo "<a href='albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}'>Click here if the browser is not automatically redirecting in {$redirectTime} seconds.</a>";
+} else {
+
+    switch($_REQUEST['submitButton']) {
+    case 'Copy':
+        checkAlbumSelected($currProfile, $currAlbum, $redirectURL, $redirectTime);
+        copyToAlbum($mysqli, $currProfile ,transformGalleryURL($_REQUEST['gallery']), $_REQUEST['selectedAlbum'] );
+
+        $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
+        $redirectTime = 24;
+        break;
+    case 'Move':
+        checkAlbumSelected($currProfile, $currAlbum, $redirectURL, $redirectTime);
+        
+        moveToAlbum($mysqli, transformGalleryID($_REQUEST['gallery']), $_REQUEST['selectedAlbum']);
+        
+        $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
+        $redirectTime = 0;
+        break;
+    case 'Delete':
+        deleteFromAlbum($mysqli, transformGalleryID($_REQUEST['gallery']), transformGalleryURL($_REQUEST['gallery']));
+        $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
+        $redirectTime = 0;
+        break;
+    case 'Play From First Selected':
+        $firstSelect = transformGalleryID($_REQUEST['gallery'])[0];
+        $redirectTime = 0;
+        $redirectURL= "profiles.php?profileid={$currProfile}&albumname={$currAlbum}&startpicture={$firstSelect}";
+        break;
+    }
+}
+
+function moveToAlbum($connect, $tableIDs, $album) {
+
+    $query = "UPDATE profile_data SET album=? WHERE id=?";
+    $stmt = $connect->prepare($query);
+
+    $stmt->bind_param('si', $album, $id);
+
+    foreach($tableIDs as $id) {
+        $stmt->execute();
+    }
+    $stmt->close();
+
+}
+
+function deleteFromAlbum($connect, $tableIDs, $tableURLs) {
+    $query = "DELETE FROM profile_data WHERE id=?";
+    $stmt = $connect->prepare($query);
+
+    $stmt->bind_param('i', $id);
+
+    foreach($tableURLs as $url) {
+        unlink($url);
+    }
+
+    foreach($tableIDs as $id) {
+        $stmt->execute();
+    }
+    $stmt->close();
+    
+}
+
+function copyToAlbum($connect, $currProfile, $tableURLs, $album) {
+    $query = "INSERT INTO profile_data (profile_id, type, url, album, tag) VALUES (?, \"picture\", ?, ?, \"\")";
+    $stmt = $connect->prepare($query);
+
+    $stmt->bind_param('iss', $currProfile, $newurl, $album);
+    
+    $newurls = [];
+    foreach($tableURLs as $oldurl) {
+        $newname = pathinfo($oldurl, PATHINFO_DIRNAME) . "/" . uniqid("copy") . ".". pathinfo($oldurl, PATHINFO_EXTENSION);
+
+        copy($oldurl, $newname);
+        array_push($newurls, $newname);
+    }
+
+    foreach($newurls as $newurl) {
+
+        print_r($currProfile);
+        print_r($newurl);
+        print_r($album);
+        $stmt->execute();
+    }
+
+    $stmt->close();
+}
+
+/*
+print_r($_SESSION['galleryDataID']);
+print_r($_SESSION['galleryDataURL']);
+print_r($_REQUEST['gallery']);
+print_r(transformGallery($_REQUEST['gallery']));
+/*
+print_r($_REQUEST);
+
+print_r($redirectURL);
+*/
+/*
+//function to copy images from one album to another
+function copyAlbum($connect){
+    $profile = $_GET['profileid'];
+    $album = $_GET['albumname'];
+    $copyFile="path/filename";//might need to change this
+    $copied = copy($album, $copyfile);
+    if(!$copied){
+        echo "Copy Failed";
+    }
+    else{
+        echo "Copy Successful";
+    }
+}
+
+//function to move images from album to another
+function moveAlbum($connect, $file, $to){
+    $profile = $_GET['profileid'];
+    $album = $_GET['albumname'];
+    $file = $album;
+    $path_parts = pathinfo($file);
+    $newplace = "$to/{$path_parts['basename']}";
+    if(rename($file, $newplace))
+    {
+        return $newplace;
+    }
+    return null;
+}
+
+//functio to move files from one album to another
+function moveAlbum2($connect){
+    $profile = $_GET['profileid'];
+    $album = $_GET['albumname'];
+    $moveFile="path/filename";//might need to change this
+    $moved = copy($album, $moveFile);
+    if(!moved){
+       echo "Moved Failed";
+    }
+    else{
+        unlink($album);
+        echo "Move Successfull";
+    }
+}
+*/
+?>
+
+<meta http-equiv="refresh" content="<?php echo $redirectTime; ?>; URL='<?php echo $redirectURL; ?>'"/> 
