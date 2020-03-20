dfe024672439f88ffe93d096068f35567a448f63
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-03-20 12:37:32 -0400

    Update logout.php

--- staff/logout.php
+++ staff/logout.php
@@ -6,5 +6,5 @@ session_unset();
 $_SESSION['action1']="You have logged out successfully..!";
 ?>
 <script language="javascript">
-document.location="adminlogin.php";
+document.location="stafflogin.php";
 </script>
