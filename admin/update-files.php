815ffd43900a9393324b101ac0dda9fa8ad9135d
Mohammad Shahriyar <100587434@oncampus.local>
2020-03-19 23:36:14 -0400

    testing

--- admin/update-files.php
+++ admin/update-files.php
@@ -2,8 +2,6 @@
 session_start();
 include'dbconnection.php';
 //Checking session is valid or not
-require_once('dbconfig/config.php');
-require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = Aws\S3\S3Client::factory();
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
