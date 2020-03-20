2f57c3e804f4e1cf3e03425f50a7d5319fce7e34
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-03-20 01:09:58 -0400

    Update new-caregiver.php

--- admin/new-caregiver.php
+++ admin/new-caregiver.php
@@ -170,7 +170,7 @@
 							<div class="form-group">
 								<label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Select Patient</label>
 								<div class="col-sm-10">
-								<select name="name">
+								<select name="patient_name">
 								<?php $ret=mysqli_query($con,"select * from patient");
 								while($row=mysqli_fetch_array($ret))
 								{
@@ -179,7 +179,7 @@
 								}
 								?>
 							</select>
-							<input id="patient_name" name="patient_name" type="text" value="test">
+						
 						</div>
 							</div>
 
@@ -197,18 +197,5 @@
       </section></section>
 
 	</div>
-	<script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="assets/js/jquery.scrollTo.min.js"></script>
-    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="assets/js/common-scripts.js"></script>
-	<script>  
-     $(document).ready(function(){
-    $('select[name=name]').change(function(){
-	document.getElementById("patient_name").value = "This is a testing";
-	}
-	 }
-	</script>
 </body>
 </html>
