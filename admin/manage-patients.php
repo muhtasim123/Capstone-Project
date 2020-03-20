4b427393a61cc1e8e2d1239433eade505f0a573b
Mohammad Shahriyar <100587434@oncampus.local>
2020-03-20 01:34:21 -0400

    testing

--- admin/manage-patients.php
+++ admin/manage-patients.php
@@ -132,13 +132,17 @@ include'dbconnection.php';
 								  
 									<a href="">
                                      <button class="btn btn-success btn-xs"><i class="fa fa-play"></i></button></a>
+									 
                                      <a href="https://ontario-shores.herokuapp.com/update-files.php?uid=<?php echo $row['id'];?>">
                                      <button class="btn btn-primary btn-xs"><i class="fa fa-upload"></i></button></a>
+									 
 									 <a href="update-profile.php?uid=<?php echo $row['id'];?>">
                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+									 
+									 <a href="new-caregiver.php?uid=<?php echo $row['id'];?>">
+                                     <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+									 
                                      <a href="manage-patients.php?id=<?php echo $row['id'];?>">
-									 <button class="btn btn-primary btn-xs"><i class="fa fa-plus"></i></button></a>
-                                     <a href="new-caregiver.php?id=<?php echo $row['id'];?>">
                                      <button class="btn btn-danger btn-xs" onClick="return confirm('Do you really want to delete');"><i class="fa fa-trash-o "></i></button></a>
                                   </td>
                               </tr>
