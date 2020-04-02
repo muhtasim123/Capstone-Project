212fea4aae4b06db4122576538ac5bafa5600472
Mohammad Shahriyar <100587434@oncampus.local>
2020-04-02 15:26:18 -0400

    testing

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -146,14 +146,14 @@
                                <div class="form-group">
                               <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Password</label>
                               <div class="col-sm-10">
-                                  <input type="text" placeholder="Enter Password" class="form-control" name="password" required>
+                                  <input type="password" placeholder="Enter Password" class="form-control" name="password" required>
                               </div>
                           </div>
 						  
 						  <div class="form-group">
                               <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Confirm Password</label>
                               <div class="col-sm-10">
-                                  <input type="text" placeholder="Enter Password" class="form-control" name="cpassword" required >
+                                  <input type="password" placeholder="Enter Password" class="form-control" name="cpassword" required >
                               </div>
                           </div>
 						  
