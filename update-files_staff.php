f12885fc7fbde9493ddb0310fc57b78d26930262
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-04-03 11:37:36 -0400

    Update update-files_staff.php

--- update-files_staff.php
+++ update-files_staff.php
@@ -8,6 +8,10 @@ require('vendor/autoload.php');
 $s3 = Aws\S3\S3Client::factory();
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 
+if($_SESSION['login']!="1"){
+	header( "Location: staff/stafflogin.php");
+}
+
 // for updating user info
 if(isset($_POST['upload']))
 {
