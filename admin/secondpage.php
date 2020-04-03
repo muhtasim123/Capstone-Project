c7548f35a2e7fbee8ee9f42204e3aec2a30b1173
Mohammed Hameeduddin <45569565+100587893@users.noreply.github.com>
2020-04-03 14:21:26 -0400

    Update secondpage.php

--- admin/secondpage.php
+++ admin/secondpage.php
@@ -1,4 +1,8 @@
 <?php 
+
+if($_SESSION['login']!="1"){
+header("Location: adminlogin.php");}
+
 session_start();
 //creating a connection
 $mysqli = new mysqli("us-cdbr-iron-east-04.cleardb.net", "bc9da719e482f3", "deea7ef6", "heroku_dbefbfd5b04ac35");
