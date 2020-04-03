2f41f5537ce5e096363645becaed5a52a0aefdd0
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-04-03 11:40:19 -0400

    Update update-files.php

--- update-files.php
+++ update-files.php
@@ -7,7 +7,9 @@ require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = Aws\S3\S3Client::factory();
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-
+if($_SESSION['login']!="1"){
+	header( "Location: admin/adminlogin.php");
+}
 // for updating user info
 if(isset($_POST['upload']))
 {
