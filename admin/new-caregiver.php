43222cb22ff57b68b2ed1ab7ce502e361337040a
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-04-03 11:49:47 -0400

    Update new-caregiver.php

--- admin/new-caregiver.php
+++ admin/new-caregiver.php
@@ -88,7 +88,7 @@ if($_SESSION['login']!="1"){
               <ul class="sidebar-menu" id="nav-accordion">
 
               	  <p class="centered"><a href="#"><img src="assets/img/logo100.png" width="125"></a></p>
-              	  <h5 class="centered"><?php echo $_SESSION['login'];?></h5>
+              
 				  
                   <li class="sub-menu">
                       <a href="manage-patients.php" >
