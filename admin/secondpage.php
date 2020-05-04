a4962ad8267b68271c7d8627155ca610d3f60694
Danny Lionel <32670499+DannyLionel@users.noreply.github.com>
2020-05-04 13:16:26 -0400

    Update secondpage.php

--- admin/secondpage.php
+++ admin/secondpage.php
@@ -9,12 +9,6 @@ header("Location: adminlogin.php");}
 
 //Danny comment out lines 11-17
 //creating a connection
-$mysqli = new mysqli("us-cdbr-iron-east-04.cleardb.net", "bc9da719e482f3", "deea7ef6", "heroku_dbefbfd5b04ac35");
-
-checking connection
-if($mysqli->connect_error){
-    die("Connection failed: " . $mysqli->connect_error);
-}
 
 $currAlbum = $_REQUEST['currentAlbum'];
 $currProfile = $_REQUEST['currentProfileID'];
