1c98ca64583e06a4eaa2fa189291b1447213f555
Danny Lionel <32670499+DannyLionel@users.noreply.github.com>
2020-05-05 18:57:55 -0400

    Update nextpage.php

--- nextpage.php
+++ nextpage.php
@@ -1,155 +1,517 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php 
-session_start();
+
+<?php
+
+
+
+
+
+
+
+
+
 //include'dbconnection.php';
+
+
 //if($_SESSION['login']!="1"){
+
+
 //header("Location: caregiverlogin.php");}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //session_start();
+
+
 //creating a connection
-//$mysqli = new mysqli("us-cdbr-iron-east-04.cleardb.net", "bc9da719e482f3", "deea7ef6", "heroku_dbefbfd5b04ac35");
+
+
+$mysqli = new mysqli("us-cdbr-iron-east-04.cleardb.net", "bc9da719e482f3", "deea7ef6", "heroku_dbefbfd5b04ac35");
+
+
+
+
+
+
 
 //checking connection
-//if($mysqli->connect_error){
-    //die("Connection failed: " . $mysqli->connect_error);
-//}
-
-$currAlbum = $_REQUEST['currentAlbum'];
-$currProfile = $_REQUEST['currentProfileID'];
 
 
-$redirectURL = "/";
-$redirectTime = 3;
+if($mysqli->connect_error){
+
+
+   die("Connection failed: " . $mysqli->connect_error);
+
+
+}
+
+
+
+
+
+
+
+session_start();
+
+
+$curralbm = $_REQUEST['currentAlbum'];
+
+
+$currprof = $_REQUEST['currentProfileID'];
+
+
+
+
+
+
+
+
+
+
+
+
+$redirurl = "/";
+
+
+$redirtime = 3;
+
+
+
+
+
+
 
 function checkAlbumSelected($currprof, $curralbm, &$redirurl, &$redirtime) {
-    if(!isset($_REQUEST['selectedAlbum'])) {
-        $redirurl = "albumgallery.php?profileid={$currprof}&albumname={$curralbm}";
-        $redirtime = 5;
-        
-        echo "<p>No target album was selected. Going back.</p>";
-        echo "<br/>";
-        echo "<a href='albumgallery.php?profileid={$currProfile}&albumname={$curralbm}'>Click here if the browser is not automatically redirecting in {$redirectTime} seconds.</a>";
-    }
+
+
+   if(!isset($_REQUEST['selectedAlbum'])) {
+
+
+       $redirurl = "albumgallery.php?profileid={$currprof}&albumname={$curralbm}";
+
+
+       $redirtime = 5;
+
+
+      
+
+
+       echo "<p>No target album was selected. Going back.</p>";
+
+
+       echo "<br/>";
+
+
+       echo "<a href='albumgallery.php?profileid={$currprof}&albumname={$curralbm}'>Click here if the browser is not automatically redirecting in {$redirtime} seconds.</a>";
+
+
+   }
+
+
 }
 
+
+
+
+
+
+
 function transformGalleryID($gallerySelected) {
-    return array_map(function($galleryid) {
-        return $_SESSION['galleryDataID'][$galleryid];
-    }, $gallerySelected);
-    
+
+
+   return array_map(function($galleryid) {
+
+
+       return $_SESSION['galleryDataID'][$galleryid];
+
+
+   }, $gallerySelected);
+
+
+  
+
+
 }
+
+
+
+
+
+
+
+
+
+
 
 
 function transformGalleryURL($gallerySelected) {
-    return array_map(function($galleryid) {
-        return $_SESSION['galleryDataURL'][$galleryid];
-    }, $gallerySelected);
-    
+
+
+   return array_map(function($galleryid) {
+
+
+       return $_SESSION['galleryDataURL'][$galleryid];
+
+
+   }, $gallerySelected);
+
+
+  
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 if(!isset($_REQUEST['gallery'])) {
-    $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
-    $redirectTime = 5;
-    echo "<p>No image was selected. Going back.</p>";
-    echo "<br/>";
-    echo "<a href='albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}'>Click here if the browser is not automatically redirecting in {$redirectTime} seconds.</a>";
+
+
+   $redirurl = "albumgallery.php?profileid={$currprof}&albumname={$curralbm}";
+
+
+   $redirtime = 5;
+
+
+   echo "<p>No image was selected. Going back.</p>";
+
+
+   echo "<br/>";
+
+
+   echo "<a href='albumgallery.php?profileid={$currprof}&albumname={$curralbm}'>Click here if the browser is not automatically redirecting in {$redirtime} seconds.</a>";
+
+
 } else {
 
-    switch($_REQUEST['submitButton']) {
-    case 'Copy':
-        checkAlbumSelected($currProfile, $currAlbum, $redirectURL, $redirectTime);
-        copyToAlbum($mysqli, $currProfile ,transformGalleryURL($_REQUEST['gallery']), $_REQUEST['selectedAlbum'] );
 
-        $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
-        $redirectTime = 24;
-        break;
-    case 'Move':
-        checkAlbumSelected($currProfile, $currAlbum, $redirectURL, $redirectTime);
-        
-        moveToAlbum($mysqli, transformGalleryID($_REQUEST['gallery']), $_REQUEST['selectedAlbum']);
-        
-        $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
-        $redirectTime = 0;
-        break;
-    case 'Delete':
-        deleteFromAlbum($mysqli, transformGalleryID($_REQUEST['gallery']), transformGalleryURL($_REQUEST['gallery']));
-        $redirectURL = "albumgallery.php?profileid={$currProfile}&albumname={$currAlbum}";
-        $redirectTime = 0;
-        break;
-    case 'Play From First Selected':
-        $firstSelect = transformGalleryID($_REQUEST['gallery'])[0];
-        $redirectTime = 0;
-        $redirectURL= "profiles.php?profileid={$currProfile}&albumname={$currAlbum}&startpicture={$firstSelect}";
-        break;
-    }
+
+
+
+
+
+   switch($_REQUEST['submitButton']) {
+
+
+   case 'Copy':
+
+
+       checkAlbumSelected($currprof, $curralbm, $redirurl, $redirtime);
+
+
+       copyToAlbum($mysqli, $currprof ,transformGalleryURL($_REQUEST['gallery']), $_REQUEST['selectedAlbum'] );
+
+
+
+
+
+
+
+       $redirurl = "albumgallery.php?profileid={$currprof}&albumname={$curralbm}";
+
+
+       $redirtime = 24;
+
+
+       break;
+
+
+   case 'Move':
+
+
+       checkAlbumSelected($currprof, $curralbm, $redirurl, $redirtime);
+
+
+      
+
+
+       moveToAlbum($mysqli, transformGalleryID($_REQUEST['gallery']), $_REQUEST['selectedAlbum']);
+
+
+      
+
+
+       $redirurl = "albumgallery.php?profileid={$currprof}&albumname={$curralbm}";
+
+
+       $redirtime = 0;
+
+
+       break;
+
+
+   case 'Delete':
+
+
+       deleteFromAlbum($mysqli, transformGalleryID($_REQUEST['gallery']), transformGalleryURL($_REQUEST['gallery']));
+
+
+       $redirurl = "albumgallery.php?profileid={$currprof}&albumname={$curralbm}";
+
+
+       $redirtime = 0;
+
+
+       break;
+
+
+   case 'Play From First Selected':
+
+
+       $firstSelect = transformGalleryID($_REQUEST['gallery'])[0];
+
+
+       $redirtime = 0;
+
+
+       $redirurl= "profiles.php?profileid={$currprof}&albumname={$curralbm}&startpicture={$firstSelect}";
+
+
+       break;
+
+
+   }
+
+
 }
+
+
+
+
+
+
 
 function moveToAlbum($connect, $tableIDs, $album) {
 
-    $query = "UPDATE new_media SET album=? WHERE id=?";
-    $stmt = $connect->prepare($query);
 
-    $stmt->bind_param('si', $album, $id);
 
-    foreach($tableIDs as $id) {
-        $stmt->execute();
-    }
-    $stmt->close();
+
+
+
+
+   $query = "UPDATE new_media SET album=? WHERE id=?";
+
+
+   $stmt = $connect->prepare($query);
+
+
+
+
+
+
+
+   $stmt->bind_param('si', $album, $id);
+
+
+
+
+
+
+
+   foreach($tableIDs as $id) {
+
+
+       $stmt->execute();
+
+
+   }
+
+
+   $stmt->close();
+
+
+
+
+
+
 
 }
+
+
+
+
+
+
 
 function deleteFromAlbum($connect, $tableIDs, $tableURLs) {
-    $query = "DELETE FROM new_media WHERE id=?";
-    $stmt = $connect->prepare($query);
 
-    $stmt->bind_param('i', $id);
 
-    foreach($tableURLs as $url) {
-        unlink($url);
-    }
+   $query = "DELETE FROM new_media WHERE id=?";
 
-    foreach($tableIDs as $id) {
-        $stmt->execute();
-    }
-    $stmt->close();
-    
+
+   $stmt = $connect->prepare($query);
+
+
+
+
+
+
+
+   $stmt->bind_param('i', $id);
+
+
+
+
+
+
+
+   foreach($tableURLs as $url) {
+
+
+       unlink($url);
+
+
+   }
+
+
+
+
+
+
+
+   foreach($tableIDs as $id) {
+
+
+       $stmt->execute();
+
+
+   }
+
+
+   $stmt->close();
+
+
+  
+
+
 }
 
-function copyToAlbum($connect, $currProfile, $tableURLs, $album) {
-    $query = "INSERT INTO new_media (patientid, type, link, album, tags) VALUES (?, \"picture\", ?, ?, \"\")";
-    $stmt = $connect->prepare($query);
 
-    $stmt->bind_param('iss', $currProfile, $newurl, $album);
-    
-    $newurls = [];
-    foreach($tableURLs as $oldurl) {
-        $newname = pathinfo($oldurl, PATHINFO_DIRNAME) . "/" . uniqid("copy") . ".". pathinfo($oldurl, PATHINFO_EXTENSION);
 
-        copy($oldurl, $newname);
-        array_push($newurls, $newname);
-    }
 
-    foreach($newurls as $newurl) {
 
-        print_r($currProfile);
-        print_r($newurl);
-        print_r($album);
-        $stmt->execute();
-    }
 
-    $stmt->close();
+
+function copyToAlbum($connect, $currprof, $tableURLs, $album) {
+
+
+   $query = "INSERT INTO new_media (patientid, type, link, album, tags) VALUES (?, \"picture\", ?, ?, \"\")";
+
+
+   $stmt = $connect->prepare($query);
+
+
+
+
+
+
+
+   $stmt->bind_param('iss', $currprof, $newurl, $album);
+
+
+  
+
+
+   $newurls = [];
+
+
+   foreach($tableURLs as $oldurl) {
+
+
+       $newname = pathinfo($oldurl, PATHINFO_DIRNAME) . "/" . uniqid("copy") . ".". pathinfo($oldurl, PATHINFO_EXTENSION);
+
+
+
+
+
+
+
+       copy($oldurl, $newname);
+
+
+       array_push($newurls, $newname);
+
+
+   }
+
+
+
+
+
+
+
+   foreach($newurls as $newurl) {
+
+
+
+
+
+
+
+       print_r($currprof);
+
+
+       print_r($newurl);
+
+
+       print_r($album);
+
+
+       $stmt->execute();
+
+
+   }
+
+
+
+
+
+
+
+   $stmt->close();
+
+
 }
+
+
+
+
+
+
 
 ?>
 
-<meta http-equiv="refresh" content="<?php echo $redirectTime; ?>; URL='<?php echo $redirectURL; ?>'"/>  
- </body>
-</html>
+
+
+
+
+
+
+<meta http-equiv="refresh" content="<?php echo $redirtime; ?>; URL='<?php echo $redirurl; ?>'"/>
+
